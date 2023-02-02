3338e6f79855b0eea0fca3f064daf2660185fe28
Alejandro Garcia <agarciacoding@gmail.com>
2023-02-02 11:25:53 -0600

    Initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+//todo - Declare Variables
+
+
+        //*import function/routs
+
+
+
+
+//todo - Connect to Database
+
+
+//todo - Set Middleware
+
+
+//todo - Set Routes
+
+//todo - Start Server
