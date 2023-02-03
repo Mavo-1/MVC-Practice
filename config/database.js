f2f8544679b000d9ceb43c802be97a9480b5092b
Alejandro Garcia <agarciacoding@gmail.com>
2023-02-03 11:09:13 -0600

    updates

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+    try {                                   //PROCESS.ENV REFERENCES AN ENVIRONMENT VARIABLE
+        const conn = await mongoose.connect(process.env.DB_CONNECTION)
+    } catch (error) {
+        console.log(error)
+        process.exit(1)
+
+    }
+}
+
+module.exports= connectDB
